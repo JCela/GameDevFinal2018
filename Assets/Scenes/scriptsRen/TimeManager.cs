3a2bec9ebee00da8398d125db91d2772a5d6e22a
Ren H <kerensahughes@gmail.com>
2018-11-01 22:04:39 -0400

    Time Test commit

---
+++ Assets/Scenes/scriptsRen/TimeManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+
+	//public float Delta;
+
+	//public float FixedDelta;
+
+	//public float TimeScale = 1;
+
+	public float slowDownFactor = 0.5f;
+
+	public float slowdownLength;
+
+	public static TimeManager instance; 
+	// Use this for initialization
+	 void Start()
+	{
+		instance = this; 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetKeyDown(KeyCode.P))
+		{
+		//	if (Time.timeScale == 1.0f)
+
+			//TimeScale = 0.5f; 
+			
+			//else
+			
+			//	Time.timeScale = 1.0f; 
+			
+		SlowDown();
+			
+			//Time.fixedDeltaTime = 0.02f * Time.timeScale; 
+		}
+		*/
+		
+		
+
+		//Delta = Time.deltaTime * TimeScale;
+	}
+
+	public void SlowDown()
+	{
+		Time.timeScale = slowDownFactor;
+		Debug.Log("TimeMan");
+	}
+
+	
+}
---
+++ Assets/Scenes/scriptsRen/cubeProtoMiove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeProtoMiove : MonoBehaviour
+{
+
+
+	public float CubeSpeed = 0.5f; 
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(0.2f * CubeSpeed, 0f,0f);
+			TimeManager.instance.SlowDown();
+		}
+	}
+}
