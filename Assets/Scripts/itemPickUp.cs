e3cbeb2084a38997026ec2e0df5d1fb5fd30bba1
Sean <sp4008@nyu.edu>
2018-11-06 10:45:35 -0500

    WIP: raycasting from player to detect item that can be picked up, raycasting from item to move towards player

---
+++ Assets/Scripts/itemPickUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//usage: put this on any pick up objects
+//intent: item will lerp to player and be visible in camera
+
+public class itemPickUp : MonoBehaviour {
+	
+	//destination towards 
+	public Vector3 destination;
+	public GameObject player;
+	public playerController playerObj;
+	public float pickUpSpeed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		destination =  new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerObj.itemHit == true)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, destination, pickUpSpeed * Time.deltaTime);
+			Debug.DrawLine(transform.position, destination, Color.magenta);
+			transform.LookAt(destination);
+		}
+	}
+}
